515cbae56dfd5c66c04dd88984ceae92bc2f15ce
TPAN <taopan@lusterinc.com>
2023-06-21 10:50:59 +0800

    组成模式：文件系统

--- DesignPattern/BehaviorPattern/ChainOfResponsibility/LeaveApproval/main.cpp
+++ DesignPattern/BehaviorPattern/ChainOfResponsibility/LeaveApproval/main.cpp
@@ -6,16 +6,15 @@ int main()
 {
 	ProjectLeader *projectLeader = new ProjectLeader;
 	projectLeader->askForLeave(2);
-
 	projectLeader->askForLeave(5);
 
 	GroupLeader *groupLeader = new GroupLeader;
 	projectLeader->setSupLeader(groupLeader);
 	projectLeader->askForLeave(5);
-
 	projectLeader->askForLeave(10);
 
 	SAFE_DELETE(groupLeader);
 	SAFE_DELETE(projectLeader)
-		return 0;
+
+	return 0;
 }
